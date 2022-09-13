9a33fe6531ee7153ad7b212717517d6d4319b733
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-09-13 13:34:50 +0100

    first commit

---
+++ src/test/java/com/lotachukwu/onlinevotingsystem/OnlinevotingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotachukwu.onlinevotingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinevotingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
